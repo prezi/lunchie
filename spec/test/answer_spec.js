9a01104481f202119d6a0bc91225dbd8fe7e54ae
Liana Lo <lianalo@Lianas-MacBook-Pro.local>
2014-10-15 22:26:23 +0200

    working on answer.js

---
+++ spec/test/answer_spec.js
@@ -0,0 +1,33 @@
+
+
+
+
+
+// var assert = require('assert')
+
+// date_list = require('./../../lib/answer').date_list;
+
+// suite('answer.js', function() {
+
+//   test('date_list', function() {
+
+//     // no check to see if employee actually exists. someone could spam random users
+//     // assume that no one is malicious
+//     assert.equal("Data successfully recorded.", business_logic('Lilo;14:00'));
+//     // assert.equal("Data successfully recorded.", business_logic('Lilo;12:29')); // should this work? 
+//     assert.equal("Data successfully recorded.", business_logic('Lilo;12:30')); // edge case
+//     assert.equal("Data successfully recorded.", business_logic('Lilo;14:59')); // edge case
+
+//     assert.equal("Data successfully recorded.", business_logic('Lilo;14:00'));
+//     assert.equal("Data successfully recorded.", business_logic('Matyas;14:20'));
+//     assert.equal("Data successfully recorded.", business_logic('!!!;14:20'));
+//     assert.equal(undefined, business_logic('Lilo;15:00'));
+//     assert.equal(undefined, business_logic('Lilo;12:00'));
+//     assert.equal(undefined, business_logic('Matyas;140:00'));
+//     assert.equal(undefined, business_logic('Matyas;14:2980'));
+//     assert.equal(undefined, business_logic('Matyas;140:2980'));
+//   });
+
+
+// });
+
