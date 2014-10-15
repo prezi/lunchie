5edf44f5e131b3091ad48ec6be57e5a018955d6f
Liana Lo <lianalo@Lianas-MacBook-Pro.local>
2014-10-15 22:00:15 +0200

    most tests

--- spec/test/collect_spec.js
+++ spec/test/collect_spec.js
@@ -1,20 +1,29 @@
 var assert = require('assert')
 
 business_logic = require('./../../lib/collect').business_logic;
-  
-
 
 suite('nextPrime', function() {
 
   test('business_logic', function() {
+
+  	// no check to see if employee actually exists. someone could spam random users
+  	// assume that no one is malicious
     assert.equal("Data successfully recorded.", business_logic('Lilo;14:00'));
-    assert.equal("Data successfully recorded.", business_logic('Lilo;14:20'));
+    // assert.equal("Data successfully recorded.", business_logic('Lilo;12:29')); // should this work? 
+    assert.equal("Data successfully recorded.", business_logic('Lilo;12:30')); // edge case
+    assert.equal("Data successfully recorded.", business_logic('Lilo;14:59')); // edge case
+
+    assert.equal("Data successfully recorded.", business_logic('Lilo;14:00'));
+    assert.equal("Data successfully recorded.", business_logic('Matyas;14:20'));
+    assert.equal("Data successfully recorded.", business_logic('!!!;14:20'));
+    assert.equal(undefined, business_logic('Lilo;15:00'));
     assert.equal(undefined, business_logic('Lilo;12:00'));
-    assert.equal(undefined, business_logic('Lilo;14:2980'));
+    assert.equal(undefined, business_logic('Matyas;140:00'));
+    assert.equal(undefined, business_logic('Matyas;14:2980'));
+    assert.equal(undefined, business_logic('Matyas;140:2980'));
   });
 
 
 });
-
 
 
