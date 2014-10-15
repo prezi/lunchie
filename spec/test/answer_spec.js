cc190260591953b64b923285090e5d84cdfdf725
Liana Lo <lianalo@Lianas-MacBook-Pro.local>
2014-10-15 22:36:25 +0200

    answer.js date_list test

--- spec/test/answer_spec.js
+++ spec/test/answer_spec.js
@@ -1,33 +1,31 @@
+var assert = require('assert')
+
+date_list = require('./../../lib/answer').date_list;
+
+suite('answer.js', function() {
+
+  test('date_list', function() {
+
+    var test_store = { Lilo: '14:00',
+      Judit: '14:00',
+      Matyas: '14:00',
+      Eniko: '14:00',
+      Ferenc: '14:00',
+      Balazs: '14:00' };
+
+    var store_result = { '14:00': [ 'Lilo', 'Judit', 'Matyas', 'Eniko', 'Ferenc', 'Balazs' ] };
+
+    var fourteen_reuslt = [ 'Lilo',
+      'Judit',
+      'Matyas',
+      'Eniko',
+      'Ferenc',
+      'Balazs' ];
+
+    assert.equal(6, date_list(test_store)['14:00'].length);
+
+  });
 
 
-
-
-
-// var assert = require('assert')
-
-// date_list = require('./../../lib/answer').date_list;
-
-// suite('answer.js', function() {
-
-//   test('date_list', function() {
-
-//     // no check to see if employee actually exists. someone could spam random users
-//     // assume that no one is malicious
-//     assert.equal("Data successfully recorded.", business_logic('Lilo;14:00'));
-//     // assert.equal("Data successfully recorded.", business_logic('Lilo;12:29')); // should this work? 
-//     assert.equal("Data successfully recorded.", business_logic('Lilo;12:30')); // edge case
-//     assert.equal("Data successfully recorded.", business_logic('Lilo;14:59')); // edge case
-
-//     assert.equal("Data successfully recorded.", business_logic('Lilo;14:00'));
-//     assert.equal("Data successfully recorded.", business_logic('Matyas;14:20'));
-//     assert.equal("Data successfully recorded.", business_logic('!!!;14:20'));
-//     assert.equal(undefined, business_logic('Lilo;15:00'));
-//     assert.equal(undefined, business_logic('Lilo;12:00'));
-//     assert.equal(undefined, business_logic('Matyas;140:00'));
-//     assert.equal(undefined, business_logic('Matyas;14:2980'));
-//     assert.equal(undefined, business_logic('Matyas;140:2980'));
-//   });
-
-
-// });
+});
 
