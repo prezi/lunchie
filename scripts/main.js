fb146ec4d23b947eaa77291401ca2fcc0a9d1161
Liana Lo <liana.lo@prezi.com>
2014-10-18 13:07:04 +0200

    updated test cases for rewritten business_logic

--- scripts/main.js
+++ scripts/main.js
@@ -15,14 +15,14 @@ business_logic = require('./../lib/collect').business_logic;
 var timeFormat = /([0-9]{2})\:([0-9]{2})/;
 
 module.exports = function(robot) {
-	robot.respond(/([0-9]{2})\:([0-9]{2})/i, function(msg){
+	robot.respond(/([0-9]{2})\:([0-9]{2})/i, function(msg) {
 		var name = msg.message.user.name;
 		var time = msg.match[1] + ":" + msg.match[2];
 
 		console.log("message is " + msg);
 		console.log("time is " + time);
 		
-		business_logic(name, time)
+		business_logic(name, time);
 		msg.reply("Okay, " + msg.message.user.name + "! I will sign you up for " + time + ".");
 	});
 
