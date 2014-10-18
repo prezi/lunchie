832c8a7e2e720bee4d4649911d7097437c8fc1e2
Liana Lo <liana.lo@prezi.com>
2014-10-18 12:26:10 +0200

    working on integrating business logic

--- scripts/lilo_main.js
+++ scripts/lilo_main.js
@@ -10,11 +10,30 @@
 // Commands:
 //   hubot 13:00
 
-var timeFormat = /^([0-9]{2})\:([0-9]{2})$/;
+
+business_logic = require('./../lib/collect').business_logic;
+var timeFormat = /([0-9]{2})\:([0-9]{2})/;
 
 module.exports = function(robot) {
-	robot.respond(/(?:\d|[01]\d|2[0-3]):[0-5]\d$/i, function(msg){
-		var time = msg.match[1];
+	robot.respond(/([0-9]{2})\:([0-9]{2})/i, function(msg){
+		var name = msg.message.user.name;
+		var time = msg.match[1] + ":" + msg.match[2];
+
+
+		// msg = JSON.stringify(msg);
+
+		// console.log("message is " + msg);
+		
+		// console.log("time is " + time);
+		
+		// console.log();
+		// console.log();
+		// console.log();
+		// console.log();
+		// console.log();
+		// console.log(time);
+		// console.log(name);
+		// business_logic(name, msg)
 		msg.reply("Okay, " + msg.message.user.name + "! I will sign you up for " + time + ".");
 	});
 
