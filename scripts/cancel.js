c98b45d4a7a8d10f242beb1a8440c761e3e3c0bc
kieutran <kieudung.tran@gmail.com>
2014-10-29 12:49:26 +0100

    Fixed export and require.

--- scripts/cancel.js
+++ scripts/cancel.js
@@ -1,12 +1,14 @@
 //Description:
   //Cancels lunch requests
 
+cancelLunch = require('./../lib/cancellunch').cancelLunch;
+
 module.exports = function(robot) {
   robot.respond(/cancel|Cancel|CANCEL/i, function(msg) {
   var mention_name = msg.message.user.mention_name;
 
   cancelLunch(mention_name);
 
-  msg.reply("Okay, " + msg.message.user.name + "! I will cancel your lunch request for" + time + ".");
-  };
+  msg.reply("Okay, " + msg.message.user.name + "! I will cancel your lunch request.");
+  });
 }
