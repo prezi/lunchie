5bfd9381897bc96dbfe3bf67349adfe1bc4b458b
kieutran <kieudung.tran@gmail.com>
2014-10-29 19:33:06 +0100

    Added script to respond to hello/hi.

--- scripts/hello.js
+++ scripts/hello.js
@@ -2,9 +2,9 @@
   //Responds to input that isn't a time or cancel
 
 module.exports = function(robot) {
-  robot.respond(/(^cancel|Cancel|CANCEL|([0-9]{2})\:([0-9]{2}))/)i, function(msg) {
+  robot.respond(/hello|hi|info/i, function(msg) {
   var mention_name = msg.message.user.mention_name;
 
-  msg.reply("Hello" + msg.message.user.name + "! This is how Lunchie works.");
+  msg.reply("Hello" + " "+ msg.message.user.name + "! Welcome to lunch-roulette. I hope you meet some new Prezilians today! Please input a preferred lunch time between 10:00 and 14:59. I will notify you of your random partners 15 minutes prior to lunch! \n You may also cancel or update your request anytime. Enjoy!");
   });
 }
