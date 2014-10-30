80dc45e42935b47bd74142aa53f17101d77bff35
kieutran <kieudung.tran@gmail.com>
2014-10-30 16:53:46 +0100

    testing connection

--- scripts/hello.js
+++ scripts/hello.js
@@ -5,6 +5,6 @@ module.exports = function(robot) {
   robot.respond(/hello|hi|info/i, function(msg) {
   var mention_name = msg.message.user.mention_name;
 
-  msg.reply("Hello" + " "+ msg.message.user.name + "! Welcome to lunch-roulette. I hope you meet some new Prezilians today! Please input a preferred lunch time between 10:00 and 14:59. I will notify you of your random partners 15 minutes prior to lunch! \n You may also cancel or update your request anytime. Enjoy!");
+  msg.reply("Hello" + " "+ msg.message.user.name + "! Welcome to lunch-roulette. I hope you meet some new Prezilians today! Please input a preferred lunch time between 10:00 and 14:59. I will notify you of your random partners 15 minutes prior to lunch! Enjoy!");
   });
 }
