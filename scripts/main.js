1e20a1b650d62ccc8caf5559246affa97ec905f0
jajce <jajcee@gmail.com>
2014-11-10 19:05:30 +0200

    Added error message on incorrect time interval

--- scripts/main.js
+++ scripts/main.js
@@ -18,9 +18,11 @@ module.exports = function(robot) {
 		var mention_name = msg.message.user.mention_name;
 		var time = msg.match[1] + ":" + msg.match[2];
 		var jid = msg.message.user.jid;
-
-		// collect('Zoe', '13:00', '12694_1283811@chat.hipchat.com');
-		collect(mention_name, time, jid);
-		msg.reply("Okay, " + msg.message.user.name + "! I will sign you up for " + time + ".");
+		if ((msg.match[1] <= 14 && msg.match[1] >= 13 && msg.match[2] < 60 && msg.match[2] >= 0) || (msg.match[1] == 12 && msg.match[2] >=30 && msg.match[2] < 60)) {
+			collect(mention_name, time, jid);
+			msg.reply("Okay, "	 + msg.message.user.name + "! I will sign you up for " + time + ".");
+		} else {
+			msg.reply(msg.message.user.name + ", Your time interval is not correct. Please input a preferred lunch time between 12:30 and 14:59.");
+		}
 	})
 }
