9427434ed0a2f417d00f3b858bb8b9a54bd64dbc
k-sami <khaled.sami@prezi.com>
2014-11-19 13:56:41 +0200

    Change the model to work in local db and Heroku db . Addin the Shuffling function

--- AppConfig.js
+++ AppConfig.js
@@ -12,8 +12,19 @@ var dbUserName = "postgres";
 var dbPassword = "root";
 var dbDialect = "postgres";
 
+// Development Mode [ working localy or on the server ]
+
+var deployment_local = 'local';
+var deployment_server = 'heroku';
+
+
+// to deploy on heroku this var's value must be changed to deployment_server
+
+var deployment_mode = deployment_local;
+//var deployment_mode = deployment_server;
 
 exports.dbName = dbName;
 exports.dbUserName = dbUserName;
 exports.dbPassword = dbPassword;
 exports.dbDialect = dbDialect;
+exports.deployment_mode = deployment_mode;
--- model.js
+++ model.js
@@ -1,13 +1,27 @@
 var Sequelize = require('sequelize');
 var appConfig = require('./AppConfig');
 
-// var sequelize = new Sequelize('postgres://kieutran@localhost/demodb1', {
-//     dialect: 'postgres'
-// })
+var sequelize;
 
-var sequelize = new Sequelize(appConfig.dbName, appConfig.dbUserName, appConfig.dbPassword, {
-  dialect: appConfig.dbDialect
-})
+if(appConfig.deployment_mode == 'local'){
+    // working with the local database
+ 
+  sequelize = new Sequelize(appConfig.dbName, appConfig.dbUserName, appConfig.dbPassword, {
+    dialect: appConfig.dbDialect
+  })
+
+}else{
+    // working with the server database
+
+  var match = process.env.HEROKU_POSTGRESQL_BRONZE_URL.match(/postgres:\/\/([^:]+):([^@]+)@([^:]+):(\d+)\/(.+)/);
+ 
+  sequelize = new Sequelize(match[5], match[1], match[2], {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+  });
+}
 
 sequelize
   .authenticate()
@@ -19,6 +33,7 @@ sequelize
     }
   })
 
+
 var User = sequelize.define('User', {
   mention_name: Sequelize.STRING,
   jid: Sequelize.STRING,
@@ -29,15 +44,15 @@ var User = sequelize.define('User', {
   updatedAt: 'last_update'
 })
 
-  sequelize
-  .sync({ force: false })
-  .complete(function(err) {
-     if (!!err) {
-       console.log('An error occurred while creating the table:', err)
-     } else {
-       console.log('It worked!')
-     }
-  })
+sequelize
+.sync({ force: false })
+.complete(function(err) {
+   if (!!err) {
+     console.log('An error occurred while creating the table:', err)
+   } else {
+     console.log('Table Synced successfully ')
+   }
+})
 
 // export connection
 module.exports.sequelize = sequelize;
