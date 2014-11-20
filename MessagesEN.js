692a0b1a4cfeb1efff069c5f5afc3c2f89de5d84
k-sami <khaled.sami@prezi.com>
2014-11-20 15:31:18 +0200

    Code refactoring - creating the utilities file

--- MessagesEN.js
+++ MessagesEN.js
@@ -3,11 +3,12 @@
  Author : Khaled Sami 
  notes : Contains all the messages that appear to the users
 */
+var utilities = require('./lib/utilities');
 
 var welcomeMsg = 'Hi {0} ! Welcome to lunch-roulette. I hope you meet some new Prezilians today! Please input a preferred lunch time between 12:30 and 14:59. I will notify you of your random partners 15 minutes prior to lunch! \n You may also cancel or update your request anytime. Enjoy!';
 var confirmUserSignUpChoice = 'Okay, {0} ! I will sign you up for {1}.';
-var invalidTimeForLunch = '{0}, Your time interval is not correct. Please input a preferred lunch time between {1} and 14:59.';
-var tooLateTimeForLunch = '{0}, Oops it is too late to sign up for lunchie today.';
+var invalidTimeForLunch = '{0}, Your time interval is not correct. Please input a preferred lunch time between 12:30 and 14:59.';
+var tooLateTimeForLunch = '{0}, Oops it is too late to sign up for this lunch time today , Please try another time.';
 var confirmUserCancelChoice = 'Okay, {0} ! I will cancel your lunch request.';
 var invalidTimeForCancel = 'Hey, {0} ! You tried to trick me. Before canceling lunch request try to request it ;) ';
 var noMatchesResponse = "Hey, @{0}! Unfortunately, nobody signed up for lunch at {1} Enjoy your meal!";
--- scripts/main.js
+++ scripts/main.js
@@ -12,61 +12,27 @@
 
 
 collect = require('./../lib/collect').collect;
+utilities = require('./../lib/utilities');
 
 var usrMsgs = require('../MessagesEN');
-var minTimeforLunch = 12 * 60 + 30 ;
-var maxTimeforLunch = 14 * 60 + 59 ;
+
 
 module.exports = function(robot) {
 	robot.respond(/\b([0-9]{2})\:([0-9]{2})\b/i, function(msg) {
-		var isCorrectTimeResult = isCorrectTimeForLunch(msg.match[1],msg.match[2]);
-		if(isCorrectTimeResult === 1){
+
+		var checkCorrectTimeForLunch = utilities.checkLunchTime(msg.match[1]+":"+msg.match[2]);
+
+		if(checkCorrectTimeForLunch === 1){
 			var mention_name = msg.message.user.mention_name;
 			var time = msg.match[1] + ":" + msg.match[2];
 			var jid = msg.message.user.jid;
 			collect(mention_name, time, jid);
 			msg.reply(usrMsgs.confirmUserSignUpChoice.format(msg.message.user.name,time));
-		} else if (isCorrectTimeResult == -1) {
+		} else if (checkCorrectTimeForLunch == 0) {
 			msg.reply(usrMsgs.tooLateTimeForLunch.format(msg.message.user.name));
 		} else {
-			msg.reply(usrMsgs.invalidTimeForLunch.format(msg.message.user.name, isCorrectTimeResult));
+			msg.reply(usrMsgs.invalidTimeForLunch.format(msg.message.user.name));
 		}
 	});
 };
 
-//returns 1 if time is correct, 
-//returns -1 if it is too late to sign up
-//returns current time rounded to higher quater if it is not too late, but it is > 12:30
-function isCorrectTimeForLunch(hours , minutes){
-	var currentDate = new Date();
-	var currentHours = currentDate.getHours();
-	var currentMinutes = currentDate.getMinutes();
-	var currentRoundedMinutes = (currentMinutes % 15 === 0) ? currentMinutes : currentMinutes + (15 - (currentMinutes % 15));
-	if (currentRoundedMinutes == 60) {
-		currentRoundedMinutes = 0;
-		currentHours++;
-	}
-	var currentTimeInMinutes = currentHours * 60 + currentRoundedMinutes;
-	var currentTimeString;
-	if (currentTimeInMinutes < minTimeforLunch) { 
-		currentTimeString = '12:30';
-	} else if (currentTimeInMinutes > minTimeforLunch && currentTimeInMinutes <= maxTimeforLunch) {
-		if (currentDate.getMinutes() < 10) {
-			currentTimeString = currentHours + ':0' + currentRoundedMinutes;
-		} else {
-			currentTimeString = currentHours + ':' + currentRoundedMinutes;
-		}
-	} else {
-		currentTimeString = -1;
-	}
-	if(Number(minutes) >= 60) {
-		return currentTimeString;
-	}
-	// convert time to minutes and compare them 
-	var requestedTimeForLunch = Number(hours) * 60 + Number(minutes);
-	var minTimeforLunchTmp = (minTimeforLunch < currentTimeInMinutes) ? currentTimeInMinutes : minTimeforLunch;
-	if(requestedTimeForLunch >= minTimeforLunchTmp && requestedTimeForLunch <= maxTimeforLunch) {
-		return 1;
-	}
-	return currentTimeString;
-};
