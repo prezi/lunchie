a2f698d4824adc180eea9188ad2b50db12b6ab6e
jajce <jajcee@gmail.com>
2014-12-02 17:15:59 +0200

    Added statistics gathering table

--- scripts/purge.js
+++ scripts/purge.js
@@ -1,15 +1,44 @@
 var sequelize = require('./../model').sequelize
+var User = require('./../model').User;
+var Statistics = require('./../model').Statistics;
 
 module.exports = function(robot) {
  
   var cron = require('cron');
   var cronJob = cron.job("00 00 * * *", function() {
-    
-  sequelize
-    .query("UPDATE users SET request_time=NULL, rounded_time=NULL WHERE request_time IS NOT NULL OR rounded_time IS NOT NULL;").success(function(myTableRows) {
-    	console.log("Removing .. Now users have no scheduled lunch time");
-	})
-  });
+  var data = '';
+  
+  User
+    .findAndCountAll({where : {request_time : {ne: null} }})
+    .success(function(users){
+      if (users.count > 0) {
+    	users.rows.forEach(function(item){
+		  data += item.mention_name + '-' + item.request_time + ',';
+		  item.request_time = null;
+		  item.rounded_time = null;
+		  item.save().success(function(err) {
+		    console.log('Successfully got data from user: ' + item.mention_name);
+		  });
+    	});
+    	var date = new Date();
+    	date.setDate(date.getDate() - 1); //-1 day because script start working at 00:00, so we need to gather statistics for a day that already passed
+    	Statistics
+    	  .build({
+    	    date: date,
+    	    data: data,
+    	    number_of_users: users.count
+    	  })
+    	  .save()
+    	  .success(function(){
+            console.log('Successfully added statistics for date ' + date);
+    	  })
+    	  .failure(function(err){
+    	  	console.log('Can not add statistics for date: ' + date, err);
+    	  });
+      }
+    }) 
+
+   });
 
   cronJob.start();
 }
