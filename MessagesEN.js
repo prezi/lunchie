f459a87bd5e9027b674f2be2453b26f468143e89
kieutran <kieudung.tran@gmail.com>
2014-12-03 13:51:43 +0100

    Modified acceptance message to make it more clear that users will be notified 15 minutes before lunch.

--- MessagesEN.js
+++ MessagesEN.js
@@ -6,7 +6,7 @@
 var utilities = require('./lib/utilities');
 
 exports.instructionMsg = 'Hello {0} !\nThis is how to use me!\n\nSign up: Input a preferred lunch time between 12:30 and 14:59 in hh:mm format at least 16 minutes before the actual time.\n\nUpdate: Simply input a new preferred lunch time between 12:30 and 14:59 in hh:mm format\n\nCancel: Send cancel\n\nEnjoy!';
-exports.confirmUserSignUpChoice = 'Okay, {0}! I will sign you up for {1}.';
+exports.confirmUserSignUpChoice = 'Okay, {0}! I will sign you up for {1}, and notify you of your lunch partners 15 minutes before your lunchtime.';
 exports.invalidTimeForLunch = '{0}, your time interval is not correct. Please input a preferred lunch time between {1} and 14:59.';
 exports.confirmUserCancelChoice = 'Okay, {0}! I will cancel your lunch request.';
 exports.invalidTimeForCancel = 'Hey, {0}! You tried to trick me. Before canceling lunch request try to request it ;) ';
