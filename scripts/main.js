84dbf39eb78b1f98846b40ec060446e0bba557d4
Zoe <zoe.rimay@gmail.com>
2015-01-10 14:30:02 +0100

    Update main.js
    removed extra }

--- scripts/main.js
+++ scripts/main.js
@@ -29,7 +29,6 @@ function matchCommand(msg, inputCommand) {
         showThanksMessage(inputCommand, msg);
     } else if (inputCommand.match(globals.rulesRegex) !== null) {
         // replying with the rules
-    }
     } else {
         return 0;
     }
